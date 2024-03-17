2bb793f16875bc7347e0289a0858279d014e291d
solomon02024 <163595890+solomon02024@users.noreply.github.com>
2024-03-17 12:49:32 -0300

    Update shop and gg.tanks.py

--- shop and gg.tanks.py
+++ shop and gg.tanks.py
@@ -91,3 +91,28 @@ def print_tank_position():
 
 # Главный игровой цикл
 while True:
+class Tankpiry:
+    def __init__(self, initial_amount=0):
+        self.balance = initial_amount
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+        else:
+            print("Недостаточно средств")
+
+    def check_balance(self):
+        print("Баланс танкпиров: ", self.balance)
+
+
+# Пример использования
+
+tankpiry_account = Tankpiry(100)  # Создание счета с начальным балансом 100 танкпиров
+tankpiry_account.check_balance()  # Проверяем баланс
+tankpiry_account.deposit(50)  # Вносим 50 танкпиров
+tankpiry_account.check_balance()  # Проверяем обновленный баланс
+tankpiry_account.withdraw(30)  # Снимаем 30 танкпиров
+tankpiry_account.check_balance()  # Проверяем оставшийся баланс
